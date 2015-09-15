150a6e5464f622d3fde5a42a582135c81c1d837e
myrtletree33 <me@joeltong.org>
2015-09-16 00:26:29 +0800

    serving on local port 80

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import threading, os, sys
 
 def serve_frontend():
     print 'Serving frontend..'
-    thread = threading.Thread(target = lambda: os.system('cd public/; python -m SimpleHTTPServer 3000' + target))
+    thread = threading.Thread(target = lambda: os.system('cd public/; python -m SimpleHTTPServer 80' + target))
     thread.start()
 
 def run_server(target):
