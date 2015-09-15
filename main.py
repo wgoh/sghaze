5fb87c9900f161581708fe5f3e8a0cc7d087a60b
myrtletree33 <me@joeltong.org>
2015-09-16 00:20:58 +0800

    added frontend and script to serve it

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 
 import threading, os, sys
 
+def serve_frontend():
+    print 'Serving frontend..'
+    thread = threading.Thread(target = lambda: os.system('cd public/; python -m SimpleHTTPServer 3000' + target))
+    thread.start()
+
 def run_server(target):
     print 'Started server..'
     thread = threading.Thread(target = lambda: os.system('python server.py ' + target))
@@ -15,5 +20,6 @@ def update():
 
 if  __name__ == '__main__':
     target = sys.argv[1] if len(sys.argv) == 2 else ''
+    serve_frontend()
     run_server(target)
     update()
---
+++ public/main.js
@@ -0,0 +1,6 @@
+$.getJSON( "http://128.199.251.33:8000/", function( data  ) {
+      $('.psi-value').text(data[0].psi);
+        var timestamp = moment.unix(data[0].timestamp).format("dddd, MMMM Do YYYY, ha");
+          $('.timestamp').text(timestamp);
+
+} );
