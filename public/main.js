0651c598fb36957aff0d56c77a04a4a0da0ef4a2
wgoh <a0097773@u.nus.edu>
2015-10-16 13:13:07 +0800

    added psi status

--- public/main.js
+++ public/main.js
@@ -1,9 +1,22 @@
 
 $(document).ready(function() {
-$.getJSON( "http://128.199.251.33:8000/", function( data  ) {
-      $('.psi-value').text(data[0].psi);
-        var timestamp = moment.unix(data[0].timestamp).format("dddd, MMMM Do YYYY, ha");
-          $('.timestamp').text(timestamp);
+	$.getJSON( "http://128.199.251.33:8000/", function( data  ) {
+		var psiValue = data[0].psi;
+      	$('.psi-value').text(psiValue);
+    	var timestamp = moment.unix(data[0].timestamp).format("dddd, MMMM Do YYYY, ha");
+        $('.timestamp').text(timestamp);
 
-} );
+        var psiStatus = $('.psi-status');
+        if (psiValue <= 50) {
+        	psiStatus.text(" (good)");
+        } else if (psiValue <= 100) {
+			psiStatus.text(" (moderate)");
+        } else if (psiValue <= 200) {
+			psiStatus.text(" (unhealthy)");
+        } else if (psiValue <= 300) {
+			psiStatus.text(" (very unhealthy)");
+        } else {
+			psiStatus.text(" (hazardous)");
+        }
+	} );
 });
